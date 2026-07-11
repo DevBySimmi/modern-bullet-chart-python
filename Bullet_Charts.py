c3e6c117d0a9146b82279cf4e6b25089e25fad3e
DevBySimmi <fullofwebsite.17@gmail.com>
2026-07-11 23:59:01 +0530

    First commit

---
+++ Bullet_Charts.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+categories = ['Category']
+values =[75]
+ranges = [(50, 100)]  # Range for the bullet chart
+markers = [80]  # Marker for the bullet chart
+fig, ax = plt.subplots(figsize=(8, 4))
+
+ax.barh(categories, values, color='lightblue', edgecolor='black')
+ax.set_xlim(0, 100)  # Set x-axis limit
+ax.set_xlabel('Value')  
+
+
+for i, (low, high) in enumerate(ranges):
+    ax.plot([low, high], [i, i], color='red', linewidth=8, label='Range')
+    ax.text(low, i, f'{low}', va='center', ha='right', color='black')
+    ax.plot([markers[i]], [i], 'ro', markersize=10, label='Marker')
+
+plt.title('Bullet Chart Example')
+plt.show()
